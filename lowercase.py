54506fe76f1af51c58d12d53ca7648196374df4a
Reuben Schrader <reubenschrader@gmail.com>
2014-11-05 07:46:53 +1300

    working cms

--- lowercase.py
+++ lowercase.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, flash, redirect, url_for, jsonify, Markup
-import os, pymongo, re
+import os, pymongo, re, arrow
 from pymongo import MongoClient
 from flask.ext.login import LoginManager
 from classes import newPost
@@ -22,9 +22,12 @@ content = db["content"]
 # For making url slugs
 _punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
 
+# Page type options
+page_types = ['Blog', 'Making']
+
 @app.route('/')
 def home():
-    posts = content.find()
+    posts = content.find().limit(5)
     return render_template('main.html',
         posts = posts
         )
@@ -47,9 +50,9 @@ def makings():
 def single_post(url_slug):
     for i in content.find({'url_slug': url_slug}):
         post = i
-        if post['page_type'] == 'blog':
+        if post['page_type'] == 'Blog':
             return render_template('blogpost.html', post = post)
-        elif post['page_type'] == 'making':
+        elif post['page_type'] == 'Making':
             return render_template('makingpost.html', post = post)
 
 @app.route('/cms')
@@ -59,29 +62,54 @@ def cms():
         pages = pages
         )
 
+# TODO - make saving as draft work, and save and continue editing
 @app.route('/newpost', methods=['GET', 'POST'], defaults={'url_slug': None})
 @app.route('/post/<url_slug>/edit', methods=['GET', 'POST'])
 def new(url_slug):
-    form = newPost()
     if url_slug:
         for i in content.find({'url_slug': url_slug}):
             post = i
+        publication_time = arrow.get(post["publication_time"])
     else:
         post = None
-    if form.validate_on_submit():
-        updates = {}
-        for k in form.data:
-            field_name = k
-            field_data = form.data[k]
-            updates[str(field_name)] = field_data
-        url_slug = slugify(form.title.data)
-        updates['url_slug'] = url_slug
-        content.insert(updates)
+        publication_time = arrow.now()
+    if request.method == 'POST':
+        request.get_data()
+        form = request.form
+        updates = handle_new_data(form, post)
+        if post:
+            content.update(
+                {'_id': post["_id"]},
+                {"$set":updates},
+                upsert=True
+                )
+        else:
+            content.insert(updates)
         return redirect(url_for('cms'))
     return render_template("newpost.html",
-        form = form,
-        post = post
+        post = post,
+        page_types = page_types,
+        publication_time = publication_time
         )
+
+def handle_new_data(form_data, post):
+    updates = {}
+    for k in form_data:
+        if "datestamp" in k:
+            pass
+        else:
+            field_data = form_data[k]
+            updates[str(k)] = field_data
+    publication_time = arrow.get(int(form_data["datestamp_year"]), int(form_data["datestamp_month"]), int(form_data["datestamp_day"]))
+    publication_time = publication_time.datetime
+    print publication_time
+    updates['publication_time'] = publication_time
+    if post:
+        pass
+    else:
+        url_slug = slugify(form_data['title'])
+        updates['url_slug'] = url_slug
+    return updates
 
 def slugify(text, delim=u'-'):
     """Generates an ASCII-only slug. Thanks Armin Ronacher!"""
@@ -90,32 +118,27 @@ def slugify(text, delim=u'-'):
         result.extend(unidecode(word).split())
     return unicode(delim.join(result))
 
-# Got to figure out how to add stuff like this
-def rel_links(rel_link):
-    link_dict = {}
-    rel_link.rstrip().split('\n')
-    for link in rel_link:
-        link.split(',')
-        print link
-#        url = link[0]
-#        title = link[1]
-#        link_dict[url] = link
-    return link_dict
-
-@app.route('/testform', methods=['GET', 'POST'])
-def testform():
-    form = newPost()
+@app.route('/testform', methods=['GET', 'POST'], defaults={'url_slug': None})
+@app.route('/post/<url_slug>/testedit', methods=['GET', 'POST'])
+def testform(url_slug):
+    if url_slug:
+        for i in content.find({'url_slug': url_slug}):
+            post = i
+    else:
+        post = None
     updates = {}
-    if form.validate_on_submit():
-        for k in form.data:
-            field_name = k
-            field_data = form.data[k]
-            updates[str(field_name)] = field_data
-        url_slug = slugify(form.title.data)
-        updates['url_slug'] = url_slug
-    content.insert(updates)
-    return render_template("newpost.html",
-        form = form
+    request.get_data()
+    form = request.form
+#    title = request.form['title']
+    print form
+    for k in form:
+        print k
+        field_data = form[k]
+        updates[str(k)] = field_data
+    print updates
+    return render_template("testform.html",
+        post = post,
+        page_types = page_types
         )
 
 @app.errorhandler(404)
